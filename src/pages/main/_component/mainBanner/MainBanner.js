fd775ac1a4d79987b0075be680f3edfb2d57cf21
minkyong00 <minkyong131@gmail.com>
2025-04-04 17:28:24 +0900

    메인페이지 배너 이전다음 버튼 추가

--- src/pages/main/_component/mainBanner/MainBanner.js
+++ src/pages/main/_component/mainBanner/MainBanner.js
@@ -4,7 +4,16 @@ import S from './style';
 const MainBanner = () => {
   return (
     <S.container>
-      <S.Banner>배너</S.Banner>
+      <S.Banner>
+
+      </S.Banner>
+      <S.NextPrevButtonBox>
+        <S.NextPrevButton>&lt;</S.NextPrevButton>
+        <div>1</div>
+        <S.Bar>|</S.Bar>
+        <div>2</div>
+        <S.NextPrevButton>&gt;</S.NextPrevButton>
+      </S.NextPrevButtonBox>
     </S.container>
   );
 };
--- src/pages/main/_component/mainBanner/style.js
+++ src/pages/main/_component/mainBanner/style.js
@@ -3,15 +3,43 @@ import styled from "styled-components";
 const S = {};
 
   S.container = styled.div`
-    width: 1163px;
-    height: 750px;
+    width: 1100px;
+    height: 700px;
     margin: 0 auto;
+
+    position: relative;
   `
 
   S.Banner = styled.div`
     background-color: gray;
     width: 100%;
     height: 100%;
+
+    border-radius: 30px;
+  `
+
+  S.NextPrevButtonBox = styled.div`
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    width: 100px;
+    height: 40px;
+    background-color: #fff;
+    border-radius: 30px;
+  `
+
+  S.Bar = styled.div`
+    color: rgba(0, 0, 0, 0.3);
+  `
+
+  S.NextPrevButton = styled.div`
+    cursor: pointer;
   `
 
 export default S;
