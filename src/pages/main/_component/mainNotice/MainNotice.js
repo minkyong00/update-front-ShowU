8c452c3b8acc888aaf34082e50460c0ab7d7c553
minkyong00 <minkyong131@gmail.com>
2025-04-04 20:27:51 +0900

    메인페이지 뉴스소식 슬라이드 구현

--- src/pages/main/_component/mainNotice/MainNotice.js
+++ src/pages/main/_component/mainNotice/MainNotice.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import S from './style';
 
 const MainNotice = () => {
+  const [newsList, setNewsList] = useState([]); //뉴스목록
+  const [currentIndex, setCurrentIndex] = useState(0); //뉴스 인덱스
+
+  useEffect(() => {
+    const getNewsList = async () => {
+      try {
+        const response = await fetch(`http://localhost:8000/community/newsMain/`);
+        const data = await response.json();
+        setNewsList(data)
+      } catch (error) {
+        console.error("메인페이지 뉴스 목록 에러", error);
+      }
+    }
+
+    getNewsList();
+
+  }, [])
+
+  // console.log("newsList", newsList)
+
+  // 다음 뉴스보기
+  const nextNews = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % newsList.length);
+  }
+
+  const prevNews = () => {
+    setCurrentIndex((prevIndex) => 
+      prevIndex === 0 ? newsList.length - 1 : prevIndex - 1
+    )
+  }
+
   return (
     <S.Container>
       <S.Wrapper>
         <S.News>
           NEWS
         </S.News>
-        <S.ContentBox>
-          <S.Content>
-            내용
-          </S.Content>
-          <S.Date>
-            2025.02.01
-          </S.Date>
-        </S.ContentBox>
+        
+        {newsList.length > 0 && 
+          <S.ContentBox>
+            <S.Content>
+              {newsList[currentIndex].title}
+            </S.Content>
+            {/* <S.Date>
+              2025.02.01
+            </S.Date> */}
+          </S.ContentBox>}
+
         <S.NextPrevButtonBox>
-          <S.NextPrev>
+          <S.NextPrev onClick={prevNews}>
             &lt;
           </S.NextPrev>
           <S.Bar>
             | 
           </S.Bar>
-          <S.NextPrev>
+          <S.NextPrev onClick={nextNews}>
             &gt;
           </S.NextPrev>
         </S.NextPrevButtonBox>
