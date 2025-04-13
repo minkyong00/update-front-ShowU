906fc01aa877e9561bd8477d51c08826c11eabe4
minkyong00 <minkyong131@gmail.com>
2025-04-13 22:45:43 +0900

    마이페이지 미결제 내역 낙찰된 사용자 정보 불러오기

--- src/pages/mypage/myPay/paddingMyPay/PaddingComponent.jsx
+++ src/pages/mypage/myPay/paddingMyPay/PaddingComponent.jsx
@@ -5,13 +5,7 @@ import { useSelector } from 'react-redux';
 
 const PaddingComponent = ({ currentList, handleNavigate }) => {
   const { currentUser } = useSelector((state) => state.user)
-  // console.log("currentUser", currentUser._id)
-
-  console.log("미결제 내역", currentList.map((bid) => bid.bidHistory))
-  const bidHistory = currentList.map((bid) => bid.bidHistory)
-  console.log("bidHistory", bidHistory)
-  
-
+  // console.log("미결제 경매", currentList)
 
   return (
     <div>
@@ -27,32 +21,40 @@ const PaddingComponent = ({ currentList, handleNavigate }) => {
           </S.Tr>
         </S.Thead>
         <S.Tbody>
-          {currentList && currentList.map((item, i) => (
-            <React.Fragment key={i}>
-              <S.ContentTr>
-                <th scope="row">{item.bidHistory.timestamp}</th>
-                <td>
-                  {item.auctionName}
-                </td>
-                {/* <td>{item.totalAmount}</td> */}
-                <td>경매</td>
-                {/* <td>{item.status}</td> */}
-              </S.ContentTr>
-              <tr>
-                <td colSpan="6">
-                  <S.Image>
-                      <img
-                        onClick={() => handleNavigate( 
-                          `/shop/auction/detail/${item.productId._id}`
-                          )}
-                        src={item.image}
-                        alt={`상품 이미지`}
-                      />
-                  </S.Image>
-                </td>
-              </tr>
-            </React.Fragment>
-          ))}
+          {currentList
+            ?.filter(item =>
+              item.bidHistory.some(bid => bid.userId.toString() === currentUser._id.toString())
+            )
+            .map((item, i) => {
+              const latestBid = item.bidHistory
+                .filter(bid => bid.userId.toString() === currentUser._id.toString()) //낙찰된 사용자와 로그인한 사용자가 일치치
+                .at(-1); // 가장 마지막 입찰(가격이 가장 높은 입찰)
+
+              return (
+                <React.Fragment key={item._id || i}>
+                  <S.ContentTr>
+                    <th scope="row">{new Date(latestBid.timestamp).toLocaleString()}</th>
+                    <td>{item.auctionName}</td>
+                    <td>{latestBid.price.toLocaleString()}원</td>
+                    <td>경매</td>
+                    <td>미결제</td>
+                  </S.ContentTr>
+                  <tr>
+                    <td colSpan="6">
+                      <S.Image>
+                        <img
+                          onClick={() =>
+                            handleNavigate(`/shop/auction/detail/${item.productId._id}`)
+                          }
+                          src={item.image}
+                          alt="상품 이미지"
+                        />
+                      </S.Image>
+                    </td>
+                  </tr>
+                </React.Fragment>
+              );
+            })}
         </S.Tbody>
       </S.Table>
     </div>
