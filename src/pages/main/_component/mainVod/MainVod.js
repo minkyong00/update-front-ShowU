84ad365239ae967da8349c6f5356b3f936872434
minkyong00 <minkyong131@gmail.com>
2025-09-07 22:34:04 +0900

    메인 vod 백엔드 url .env로 관리

--- src/pages/main/_component/mainVod/MainVod.js
+++ src/pages/main/_component/mainVod/MainVod.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import S from './style';
 import { Link } from 'react-router-dom';
 import Aos from 'aos';
+import { API_URL } from '../../../../config.js';
 
 const MainVod = ({ mainVod, navigate }) => {
 
@@ -18,20 +19,20 @@ const MainVod = ({ mainVod, navigate }) => {
       {mainVod.length > 0 && 
         <S.VodWrapper data-aos="fade-up">
           <S.Vod1 onClick={() => navigate(`/vod/my-ShowU/video/${mainVod[0]._id}`)}>
-            <img src={`http://localhost:8000${mainVod[0].thumbnail}`} />
+            <img src={`${API_URL}${mainVod[0].thumbnail}`} />
             <div className='overlay'>
               <p>미친 바이올린 연주</p>
             </div>
           </S.Vod1>
           <S.VodRightSide>
             <S.Vod2 onClick={() => navigate(`/vod/my-ShowU/video/${mainVod[1]._id}`)}>
-              <img src={`http://localhost:8000${mainVod[1].thumbnail}`} />
+              <img src={`${API_URL}${mainVod[1].thumbnail}`} />
               <div className='overlay'>
                 <p>환상적인 바이올린 연주</p>
             </div>
             </S.Vod2>
             <S.Vod2 onClick={() => navigate(`/vod/my-ShowU/video/${mainVod[2]._id}`)}>
-              <img src={`http://localhost:8000${mainVod[2].thumbnail}`} />
+              <img src={`${API_URL}${mainVod[2].thumbnail}`} />
               <div className='overlay'>
                 <p>이거는 베이스연주</p>
               </div>
